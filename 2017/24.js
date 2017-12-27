cd2d24975e4a72be7efa2d3f0c430d7f5ae33412
Michael Foss <michael.foss@valtech.com>
2017-12-26 21:00:17 -0500

    WIP

---
+++ 2017/24.js
@@ -0,0 +1,27 @@
+FILENAME = './24.test.txt';
+// FILENAME = './24.input.txt';
+
+// Part 1
+
+class Piece {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+}
+
+class Node {
+  constructor(piece) {
+    this.piece = piece;
+    this.children = [];
+  }
+}
+
+function part1() {
+  // Walk down a tree
+  // Find the score at a leaf
+  //
+}
+
+part1();
+
