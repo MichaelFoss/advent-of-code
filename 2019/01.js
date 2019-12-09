4797dc6d1c2c26818c2857fa7a610cd6ef8b3e19
Michael Foss <michael.foss@valtech.com>
2019-12-09 09:34:17 -0500

    Add 2019-12-01 solution (part 2)

--- 2019/01.js
+++ 2019/01.js
@@ -1,18 +1,44 @@
 const fs = require('fs');
 
-// 3226488
+// 3226488 / 4836845
 const INPUT_FILE = '01.input.txt';
-// Expected test output is 2 + 2 + 654 + 33583 = 34241
+// Simple: 2 + 2 + 654 + 33583 = 34241
+// Complex: 2 + 2 + 966 + 50346 = 51316
 // const INPUT_FILE = '01.test.txt';
 
 let data = fs.readFileSync(INPUT_FILE, 'utf-8')
-  .trim()
-  .split('\n')
-  .map(line => line.trim());
+    .trim()
+    .split('\n')
+    .map(line => parseInt(line.trim()));
 
-const main = numbers => numbers
-  .map(number => Math.floor(number / 3) - 2)
-  .reduce((sum, curr) => sum + curr, 0);
+const main = numbers => {
+    const answer = [];
+
+    const getComplexFuel = number => {
+        let fuel = 0;
+        while (number > 0) {
+            number = Math.floor(number / 3) - 2;
+            if (number > 0) {
+                fuel += number;
+            }
+        };
+        return fuel;
+    };
+
+    // Simple fuel calculation
+    answer.push(numbers
+        .map(number => Math.floor(number / 3) - 2)
+        .reduce((sum, curr) => sum + curr, 0)
+    );
+
+    // Complex fuel calculation
+    answer.push(numbers
+        .map(number => getComplexFuel(number))
+        .reduce((sum, curr) => sum + curr, 0)
+    );
+
+    return answer;
+};
 
 console.log(main(data));
 
